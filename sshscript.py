e5824328d83368ae0f27b5f0d0210f822dae9646
Chris S <smithc4@email.appstate.edu>
2015-05-27 13:31:05 -0400

    Update sshscript.py

--- sshscript.py
+++ sshscript.py
@@ -11,7 +11,7 @@ that is to be visualized.
 from ftplib import FTP
 import getpass
 import StringIO
-import SpectogramVideo as s
+import SpectrogramVideo as s
 '''
 This function opens the connection and gets the files to be parsed.
 The path parameter is the directory that has the mp3 files.
@@ -23,6 +23,23 @@ def get_file(path):
     user = raw_input("Type your username.")
     passwd = getpass.getpass("Type your password.")
     session.login(user, passwd)
+    session.cwd(path)
+    #Gets the mp3 files in the passed in directory
+    match = "*.mp3"
+    count = 1
+    #Print the total number of .mp3 files that are in the directory,
+    #and go through them all
+    print "Total number of files: " + str(len(session.nlst(match)))
+    for name in session.nlst(match):
+        read = StringIO.StringIO()
+        session.retrbinary("RETR " + name, read.write)
+        #In order to print the data, change print name to
+        #print read.getvalue()
+        print name
+        print "File number: " + str(count)
+        count += 1
+        #Close the StringIO
+        read.close()
     s.create_specgrams(path)
     #Close the FTP connection
     session.quit()
