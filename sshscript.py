734b3e03fe63d26b1190b1e0e530e595931d3219
Chris S <smithc4@email.appstate.edu>
2015-05-27 12:45:31 -0400

    Create sshscript.py

---
+++ sshscript.py
@@ -0,0 +1,45 @@
+"""
+sshscript.py
+
+This program opens up a connection to SSH into the cs.appstate.edu
+server.  It will be utilized to get the bee videos for later parsing.
+
+The get_file(filename) function is used to get the audio file
+that is to be visualized.
+"""
+
+from ftplib import FTP
+import getpass
+import StringIO
+'''
+This function opens the connection and gets the files to be parsed.
+The path parameter is the directory that has the mp3 files.
+'''
+def get_file(path):
+    #Connect to the cs server with the proper credentials.
+    session = FTP()
+    session.connect("cs.appstate.edu")
+    user = raw_input("Type your username.")
+    passwd = getpass.getpass("Type your password.")
+    session.login(user, passwd)
+    #Set the current directory to the one passed in
+    session.cwd(path)
+    #Gets the mp3 files in the passed in directory
+    match = "*.mp3"
+    count = 1
+    #Print the total number of .mp3 files that are in the directory,
+    #and go through them all
+    print "Total number of files: " + str(len(session.nlst(match)))
+    for name in session.nlst(match):
+        read = StringIO.StringIO()
+        session.retrbinary("RETR " + name, read.write)
+        #In order to print the data, change print name to
+        #print read.getvalue()
+        print name
+        print "File number: " + str(count)
+        count += 1
+        #Close the StringIO
+        read.close()
+    #Close the FTP connection
+    session.quit()
+    print "Done."
