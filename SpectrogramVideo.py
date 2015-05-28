2aa2a7bf60af9f5dbd48ae4d293ccde41b81f9dc
JLStack <stackjl@appstate.edu>
2015-05-28 12:21:18 -0400

    now saves spectrograms on server

--- SpectrogramVideo.py
+++ SpectrogramVideo.py
@@ -13,18 +13,20 @@ noverlap = 1024
 
 
 def get_data(path):
-    if path.endswith(".mp3") or path.endswith(".flacc"):
-        temp = tempfile.NamedTemporaryFile(suffix=".wav")
-        sound = AudioSegment.from_mp3(path)
+    temp = tempfile.NamedTemporaryFile(suffix=".wav")
+    if path.endswith(".flac"):
+        sound = AudioSegment.from_file(path, "flac")
         sound.export(temp.name, format="wav")
-        bee_rate, bee_data = read(temp.name)
-        temp.close()
-        return bee_rate, bee_data
+    if path.endswith(".mp3"):
+        sound = AudioSegment.from_file(path, "mp3")
+        sound.export(temp.name, format="wav")
+    bee_rate, bee_data = read(temp.name)
+    temp.close()
+    return bee_rate, bee_data
 
 
 def create_videos(input_dir):
     wd = os.getcwd()
-    print wd, input_dir + "/Specgrams/"
     os.chdir(input_dir + "/Specgrams/")
     subprocess.Popen("ffmpeg -r 24 -i %05d_left.jpeg left.mp4", shell=True,
                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -40,12 +42,10 @@ def main(input_dir):
         left_index = 1
         right_index = 1
         file_index = 1
-        recordings = os.listdir(audio_dir)
-        recordings = recordings.sort()
-        for rec in recordings:
+        for rec in os.listdir(audio_dir):
             if rec.endswith(".wav"):
                 (bee_rate, bee_data) = read(audio_dir + rec)
-            elif rec.endswith(".mp3"):
+            elif rec.endswith(".flac"):
                 (bee_rate, bee_data) = get_data(audio_dir + rec)
             else:
                 print "not an audio file"
@@ -54,8 +54,16 @@ def main(input_dir):
             if not os.path.isdir(spec_dir):
                 os.makedirs(spec_dir)
 
-            plt.specgram(bee_data,  pad_to=nfft, NFFT=nfft, noverlap=noverlap, Fs=fs)
-            plt.title(os.path.splitext(rec)[0])
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.specgram(bee_data,  pad_to=nfft, NFFT=nfft, noverlap=noverlap, Fs=fs)
+            #plt.specgram(bee_data,  pad_to=nfft, NFFT=nfft, noverlap=noverlap, Fs=fs)
+            # Pxx, freqs, bins, img = plt.specgram(bee_data,  pad_to=nfft, NFFT=nfft, noverlap=noverlap, Fs=fs)
+            # print max(freqs)
+
+            # plt.specgram(bee_data,  pad_to=nfft, NFFT=nfft, noverlap=noverlap, Fs=fs)
+            ax.set_title(os.path.splitext(rec)[0])
+            ax.set_ylim(150, 500)
             if "left" in rec:
                 plt.savefig(spec_dir + "%05d_left.jpeg" % left_index)
                 plt.close()
@@ -65,9 +73,10 @@ def main(input_dir):
                 plt.close()
                 right_index += 1
             print "file", file_index, "completed"
+            file_index += 1
         create_videos(input_dir)
 
 
-if __name__ == "__main__":
-    import sys
-    main(sys.argv[1])
+    """if __name__ == "__main__":
+    import sys"""
+main("/Users/lukestack/PycharmProjects/BeeVisualization/15-04-2015Org/")
--- sshscript.py
+++ sshscript.py
@@ -4,50 +4,95 @@ sshscript.py
 This program opens up a connection to SSH into the cs.appstate.edu
 server.  It will be utilized to get the bee videos for later parsing.
 
-The get_file(filename) function is used to get the audio file
-that is to be visualized.
 """
 
-from ftplib import FTP
+from ftplib import FTP, error_perm
 import getpass
 import StringIO
-import SpectrogramVideo as s
-'''
-This function opens the connection and gets the files to be parsed.
-The path parameter is the directory that has the mp3 files.
-'''
+import tempfile
+from pydub import AudioSegment
+from scipy.io.wavfile import read as read_wav
+import matplotlib.pyplot as plt
+
+
+fs = 44100.0
+nfft = 2048
+noverlap = 1024
+
+
+def get_data_from_flac(data):
+    flac_temp = tempfile.NamedTemporaryFile(suffix=".flac")
+
+    # writes data from server to local tempfile
+    flac = open(flac_temp.name, 'w')
+    flac.write(data)
+    flac.close()
+
+    wav_temp = tempfile.NamedTemporaryFile(suffix=".wav")
+    sound = AudioSegment.from_file(flac_temp.name, "flac")
+    sound.export(wav_temp.name, format="wav")
+    bee_rate, bee_data = read_wav(wav_temp.name)
+    flac_temp.close()
+    wav_temp.close()
+    return bee_rate, bee_data
+
+
 def main(path):
-    #Connect to the cs server with the proper credentials.
+    # Connect to the cs server with the proper credentials.
     session = FTP()
     session.connect("cs.appstate.edu")
     user = raw_input("Type your username.")
     passwd = getpass.getpass("Type your password.")
     session.login(user, passwd)
     session.cwd(path)
-    """
-    #Gets the mp3 files in the passed in directory
-    match = "*.mp3"
+    try:
+        session.mkd("../Spectrograms")
+    except error_perm:
+        pass
+
+    # Gets the flac files in the passed in directory
+    match = "*.flac"
     count = 1
-    #Print the total number of .mp3 files that are in the directory,
-    #and go through them all
+
+    # Print the total number of .mp3 files that are in the directory,
+    # and go through them all
     print "Total number of files: " + str(len(session.nlst(match)))
+
+    left_index = 1
+    right_index = 1
     for name in session.nlst(match):
         read = StringIO.StringIO()
         session.retrbinary("RETR " + name, read.write)
-        #In order to print the data, change print name to
-        #print read.getvalue()
-        print name
+
+        data = read.getvalue()
+        bee_rate, bee_data = get_data_from_flac(data)
+
+        plt.specgram(bee_data, pad_to=nfft, NFFT=nfft, noverlap=noverlap, Fs=fs)
+        plt.title(name)
+        jpeg_temp = tempfile.NamedTemporaryFile(suffix=".jpeg")
+        plt.savefig(jpeg_temp.name)
+        plt.close()
+
+        spec = open(jpeg_temp.name, 'r')
+        if "left" in name:
+            session.storbinary("STOR ../Spectrograms/%05d_left.jpeg" % left_index, spec)
+            left_index += 1
+        if "right" in name:
+            session.storbinary("STOR ../Spectrograms/%05d_right.jpeg" % right_index, spec)
+            right_index += 1
+        spec.close()
+        jpeg_temp.close()
+
         print "File number: " + str(count)
         count += 1
-        #Close the StringIO
+
+        # Close the StringIO
         read.close()
-    """
-    print path
-    s.create_specgrams(path)
-    #Close the FTP connection
+
+    # Close the FTP connection
     session.quit()
     print "Done."
 
 if __name__ == "__main__":
-    import sys 
+    import sys
     main(sys.argv[1])
