e0cf4db258d28684887b7ba055288e9c5bfcff9d
JLStack <stackjl@appstate.edu>
2015-06-10 10:01:46 -0400

    switched to UTC

--- Dates.py
+++ Dates.py
@@ -2,12 +2,18 @@ __author__ = 'lukestack'
 
 from datetime import datetime, timedelta
 import time
+import pytz
 
-START_DATE = datetime.strptime('2015-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+utc = pytz.timezone('UTC')
+START_DATE = datetime(year=2015, month=1, day=1, hour=0, minute=0, second=0, tzinfo=utc)
 # Program will stop working on 2046-11-24 at 20:15:00
 
+
 def to_hex(date, file_time):
-    d2 = datetime.strptime(date + " " + file_time, '%Y-%m-%d %H:%M:%S')
+    date = date.split("-")
+    file_time = file_time.split(":")
+    d2 = datetime(year=int(date[0]), month=int(date[1]), day=int(date[2]),
+                  hour=int(file_time[0]), minute=int(file_time[1]), second=int(file_time[2]), tzinfo=utc)
     d1_ts = time.mktime(START_DATE.timetuple())
     d2_ts = time.mktime(d2.timetuple())
     minutes = int(d2_ts-d1_ts) / 60
