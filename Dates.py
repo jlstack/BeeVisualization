611fb09cd48fb5bc17188e97d20809d8991daa10
JLStack <stackjl@appstate.edu>
2015-06-11 13:17:59 -0400

    converted to seconds

--- Dates.py
+++ Dates.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 utc = pytz.timezone('UTC')
 START_DATE = datetime(year=1970, month=1, day=1, hour=0, minute=0, second=0, tzinfo=utc)
-# Program will stop working on 2480-05-19T20:15:00Z
+# Program will stop working on 2106-02-07T06:28:15Z
 
 
 def to_hex(date, file_time):
@@ -18,14 +18,14 @@ def to_hex(date, file_time):
                   hour=int(file_time[0]), minute=int(file_time[1]), second=int(file_time[2]), tzinfo=utc)
     d1_ts = time.mktime(START_DATE.timetuple())
     d2_ts = time.mktime(d2.timetuple())
-    minutes = int((d2_ts-d1_ts) / 60)
-    return '{:07x}'.format(minutes), '/'.join('{:07x}'.format(minutes)[:-1]) + "/"
+    seconds = int(d2_ts - d1_ts)
+    return '{:08x}'.format(seconds), '/'.join('{:08x}'.format(seconds)[:-1]) + "/"
 
 
 def to_date(hex_num):
-    minutes = int(hex_num, 16)
-    date = (START_DATE + timedelta(minutes=minutes)).date()
-    file_time = (START_DATE + timedelta(minutes=minutes)).time()
+    seconds = int(hex_num, 16)
+    date = (START_DATE + timedelta(seconds=seconds)).date()
+    file_time = (START_DATE + timedelta(seconds=seconds)).time()
     return str(date), str(file_time)
 
 
