7403daae23b1fc33a7741adc03af70949f86c6b6
JLStack <stackjl@appstate.edu>
2015-07-09 10:34:15 -0400

    does all times at once

--- TimeSeparator.py
+++ TimeSeparator.py
@@ -2,18 +2,24 @@ __author__ = 'lukestack'
 import os
 import shutil
 
+times = ['00', '05', '08', '12', '15', '17', '20']
 
-def main(input_dir, output_dir, time):
+
+def main(input_dir, output_dir):
     if not input_dir.endswith("/"):
         input_dir += "/"
     if not output_dir.endswith("/"):
         output_dir += "/"
-    if "pit1" in input_dir:
-        output_dir = output_dir + time + "/pit1/"
-    else:
-        output_dir = output_dir + time + "/pit2/"
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
+    for time in times:
+        if "pit1" in input_dir:
+            pit_output_dir = output_dir + "/pit1/"
+            time_output_dir = output_dir + "/pit1/" + time + "/"
+        else:
+            pit_output_dir = output_dir + "/pit2/"
+            time_output_dir = output_dir + "/pit2/" + time + "/"
+        if not os.path.isdir(time_output_dir):
+            os.makedirs(time_output_dir)
+
     directories = os.listdir(input_dir)
     directories.sort()
     for d in directories:
@@ -28,12 +34,14 @@ def main(input_dir, output_dir, time):
             recordings = os.listdir(audio_dir)
             recordings.sort()
             for rec in recordings:
-                for i in range(0, 3):
-                    t = time + ":0" + str(i) + ":"
-                    if t in rec:
-                        shutil.copyfile(audio_dir + rec, output_dir + date + '_' + rec)
+                for time in times:
+                    for i in range(0, 3):
+                        t = time + ":0" + str(i) + ":"
+                        if t in rec:
+                            print time_output_dir + date + '_' + rec
+                            shutil.copyfile(audio_dir + rec, pit_output_dir + time + '/' + date + '_' + rec)
 
 
 if __name__ == "__main__":
     import sys
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2])
