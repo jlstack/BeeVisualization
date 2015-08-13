1f6b11e551b13469febe1844681fdc37ebc6b136
JLStack <stackjl@appstate.edu>
2015-08-13 12:44:45 -0400

    added many new features including login prompt

--- Bee_Gui.py
+++ Bee_Gui.py
@@ -6,6 +6,7 @@ from matplotlib.backend_bases import key_press_handler
 import matplotlib.ticker as tkr
 import Tkinter as Tk
 import tkSimpleDialog
+import tkMessageBox
 import matplotlib.pyplot as plt
 import Dates
 import ftplib
@@ -18,9 +19,33 @@ import os
 import math
 import numpy as np
 
-user = 'stackjl'
-password = ''
-ftp = ftplib.FTP('cs.appstate.edu', user, password)
+ftp = username = password = None
+
+
+class LoginDialog(tkSimpleDialog.Dialog):
+    """
+    Pop-up box that prompts user for login information.
+    """
+    def body(self, master):
+        """
+        Creates the box.
+        :param master: root
+        :return: self.name to highlight the username input box
+        """
+        Tk.Label(master, text="username:").grid(row=0)
+        Tk.Label(master, text="password:").grid(row=1)
+        self.name = Tk.Entry(master)
+        self.password = Tk.Entry(master, show="*")
+        self.name.grid(row=0, column=1)
+        self.password.grid(row=1, column=1)
+        return self.name  # initial focus
+
+    def apply(self):
+        """
+        Retrieves all of the information and stores it in self.result
+        :return: None
+        """
+        self.result = self.name.get(), self.password.get()
 
 
 class DateDialog(tkSimpleDialog.Dialog):
@@ -33,7 +58,6 @@ class DateDialog(tkSimpleDialog.Dialog):
         :param master: root
         :return: self.date to highlight the date input box
         """
-        self.withdraw()
         Tk.Label(master, text="Date:").grid(row=0)
         Tk.Label(master, text="Time:").grid(row=1)
         self.date = Tk.Entry(master)
@@ -92,6 +116,7 @@ class BeeApp(Tk.Tk):
         self.input_dir = "/usr/local/bee/beemon/beeW/Luke/numpy_specs2/%s/" % self.pit
         self.mp3_dirs = ["/usr/local/bee/beemon/mp3/" + self.pit + "/%s/",
                          "/usr/local/bee/beemon/" + self.pit + "/%s/audio/"]
+        self.video_dir = "/usr/local/bee/beemon/" + self.pit + "/%s/video/"
         start_hex, start_dir = Dates.to_hex(date, time)
         self.current_input = self.input_dir + start_dir
         self.channel = channel
@@ -99,9 +124,9 @@ class BeeApp(Tk.Tk):
         self.center = format(int(self.leftmost, 16) + 8, 'x')
         self.zoom = 1
         self.files = self.audio_files = {'pit1': {}, 'pit2': {}}
-        self.cax = self.fig = self.ax = self.canvas = self.stream = self.combined_spec = None
+        self.cax = self.stream = self.combined_spec = None
         self.current_view = "spec"
-        self.get_next_16(self.center)
+        self.update_combined_spec(self.center)
 
         self.option_add('*tearOff', False)  # Creating a menubar
         self.menubar = Tk.Menu(self)
@@ -117,6 +142,10 @@ class BeeApp(Tk.Tk):
         channel.add_radiobutton(label="right", command=lambda: self.change_channel("right"))
 
         self.matplotlib_plot = Tk.Frame(self)  # Frame that holds the matplotlib figure
+        fig, ax = plt.subplots()
+        fig.canvas.draw()
+        self.fig = fig
+        self.ax = ax
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.matplotlib_plot)
         self.canvas.show()
         self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
@@ -125,6 +154,7 @@ class BeeApp(Tk.Tk):
         self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
         self.canvas.mpl_connect('key_press_event', self.toolbox_event)
         self.matplotlib_plot.pack(fill=Tk.BOTH, expand=True)
+        self.create_specgram()  # creates the initial spectrogram
 
         self.control_frame = Tk.Frame(self, width=400)  # Frame that holds the navigational buttons
         self.zoom_out = Tk.Button(self.control_frame, text="-", command=self.on_zoom_out)
@@ -133,14 +163,16 @@ class BeeApp(Tk.Tk):
         self.zoom_in.grid(row=0, column=1)
         if self.zoom == 1:
             self.zoom_in['state'] = 'disabled'  # Makes zoom in button unclickable
-        self.play = Tk.Button(self.control_frame, text="Play", command=self.on_play)
+        self.play = Tk.Button(self.control_frame, text="Audio", command=self.on_play)
         self.play.grid(row=0, column=2)
-        self.plot = Tk.Button(self.control_frame, text="Plot Frequencies", command=self.on_plot)
-        self.plot.grid(row=0, column=3)
+        self.play_video = Tk.Button(self.control_frame, text="Video", command=self.on_play_video)
+        self.play_video.grid(row=0, column=3)
+        self.toggle = Tk.Button(self.control_frame, text="Frequencies", command=self.on_plot)
+        self.toggle.grid(row=0, column=4)
         self.left = Tk.Button(self.control_frame, text="<", command=self.on_left)
-        self.left.grid(row=0, column=4)
+        self.left.grid(row=0, column=5)
         self.right = Tk.Button(self.control_frame, text=">", command=self.on_right)
-        self.right.grid(row=0, column=5)
+        self.right.grid(row=0, column=6)
         self.control_frame.pack()
 
         self.message_frame = Tk.LabelFrame(self, height=50, width=400)  # Frame that holds the message
@@ -173,7 +205,7 @@ class BeeApp(Tk.Tk):
             self.pit = pit
             self.leftmost = make_hex8("".join(start_dir.split("/")))
             self.center = format(int(self.leftmost, 16) + 8, 'x')
-            self.get_next_16(self.center)
+            self.update_combined_spec(self.center)
         except TypeError:
             pass
 
@@ -187,7 +219,9 @@ class BeeApp(Tk.Tk):
         self.input_dir = "/usr/local/bee/beemon/beeW/Luke/numpy_specs2/%s/" % self.pit
         self.mp3_dirs = ["/usr/local/bee/beemon/mp3/" + self.pit + "/%s/",
                          "/usr/local/bee/beemon/" + self.pit + "/%s/audio/"]
-        self.get_next_16(self.center)
+        self.video_dir = "/usr/local/bee/beemon/" + self.pit + "/%s/video/"
+        self.update_combined_spec(self.center)
+        self.update_matplotlib_fig()
 
     def change_channel(self, channel):
         """
@@ -196,50 +230,118 @@ class BeeApp(Tk.Tk):
         :return: None
         """
         self.channel = channel
-        self.get_next_16(self.center)
+        self.update_combined_spec(self.center)
+        self.update_matplotlib_fig()
 
     def on_zoom_out(self):
         """
-        Zooms out one level and calls self.get_next_16 to update the spectrogram.
+        Zooms out one level and calls self.update_combined_spec to update the spectrogram.
         :return: None
         """
         if self.zoom < 28:
             self.zoom += 1
-            self.get_next_16(self.center)
+            self.update_combined_spec(self.center)
         if self.zoom > 1:
             self.zoom_in['state'] = 'normal'
+        self.update_matplotlib_fig()
+
 
     def on_zoom_in(self):
         """
-        Zooms in one level and calls self.get_next_16 to update the spectrogram.
+        Zooms in one level and calls self.update_combined_spec to update the spectrogram.
         :return: None
         """
         if self.zoom != 1:
             self.zoom -= 1
-            self.get_next_16(self.center)
+            self.update_combined_spec(self.center)
         if self.zoom == 1:
             self.zoom_in['state'] = 'disabled'
+        self.update_matplotlib_fig()
 
     def on_plot(self):
         """
-        Creates plot of frequencies.
+        Handler for toggle button when the view is being changed
+        from a spectrogram to a frequency plot.
         :return: None
         """
-        print self.combined_spec[~np.all(self.combined_spec == 0, axis=1)].T.shape, self.combined_spec.shape
-        if self.current_view == "spec":
-            self.ax.clear()
-            freqs = np.arange(0, self.combined_spec.shape[1] / 2.0, .5)
-            if not math.isnan(np.mean(self.combined_spec[~np.all(self.combined_spec == 0, axis=1)].T)):
-                self.ax.set_ylim((0, np.amax(self.combined_spec[~np.all(self.combined_spec == 0, axis=1)].T[2:, :])))
-                # Takes max after column 2 because there is often an outlier at the first two columns
-                self.ax.plot(freqs, np.mean(self.combined_spec[~np.all(self.combined_spec == 0, axis=1)].T, axis=1))
-            else:
-                self.ax.plot(freqs, [0] * len(freqs))
-            self.canvas.draw()
-            self.plot.config(text="Spectrogram")
-            self.current_view = "plot"
+        self.toggle.config(text="Spectrogram", command=self.on_spec)
+        self.current_view = "plot"
+        self.create_plot()
+
+    def on_spec(self):
+        """
+        Handler for toggle button when the view is being changed
+        from a frequency plot to a spectrogram.
+        :return: None
+        """
+        self.toggle.config(text="Frequencies", command=self.on_plot)
+        self.current_view = "spec"
+        self.create_specgram()
+
+    def create_plot(self):
+        """
+        Creates plot of frequencies for combined_spec.
+        :return: None
+        """
+        self.ax.clear()
+        date1, time1 = Dates.to_date(self.leftmost)
+        date2, time2 = Dates.to_date('{:08x}'.format(int(self.leftmost, 16) + 2 ** (3 + self.zoom) - 1))
+        self.ax.set_title(date1 + "T" + time1 + " - " + date2 + "T" + time2)
+        freqs = np.arange(0, self.combined_spec.shape[1] / 2.0, .5)
+        if not math.isnan(np.mean(self.combined_spec[~np.all(self.combined_spec == 0, axis=1)].T)):
+            self.ax.set_ylim((0, np.amax(self.combined_spec[~np.all(self.combined_spec == 0, axis=1)].T[2:, :])))
+            # Takes max after column 2 because there is often an outlier at the first two columns
+            self.ax.plot(freqs, np.mean(self.combined_spec[~np.all(self.combined_spec == 0, axis=1)].T, axis=1))
         else:
-            self.get_next_16(self.center)
+            self.ax.plot(freqs, [0] * len(freqs))
+        self.canvas.draw()
+
+    def create_specgram(self):
+        """
+        Creates the current spectrogram for combined_spec.
+        :return: None
+        """
+        rightmost = '{:08x}'.format(int(self.leftmost, 16) + 2 ** (3 + self.zoom) - 1)
+        date1, file_time1 = Dates.to_date(self.leftmost)
+        date2, file_time2 = Dates.to_date(rightmost)
+        if not date1 == date2:
+            title = date1 + "  -  " + date2
+        else:
+            title = date1
+        self.ax.clear()
+        self.ax.set_xticks(np.arange(0, self.combined_spec.shape[0], 1.0))
+        self.ax.set_xticklabels(["" for x in range(self.combined_spec.shape[0])])
+        cax = self.ax.imshow(20 * np.log10(self.combined_spec.T), origin='lower',
+                             aspect='auto', interpolation='nearest')
+        self.ax.set_title(title)
+        labels = [item.get_text() for item in self.ax.get_xticklabels()]
+        labels[0] = file_time1
+        center_date, center_time = Dates.to_date(self.center)
+        labels[8] = center_time
+        labels[len(labels) - 1] = file_time2
+        self.ax.set_xticklabels(labels)
+        yfmt = tkr.FuncFormatter(numfmt)
+        self.ax.yaxis.set_major_formatter(yfmt)
+        # sets the colorbar for all spectrograms
+        if self.cax is None and np.count_nonzero(self.combined_spec) != 0:
+            self.cax = self.ax.imshow(20 * np.log10(self.combined_spec.T), origin='lower',
+                                      aspect='auto', interpolation='nearest')
+            self.fig.colorbar(cax)
+        if self.cax is not None:
+            cax.set_clim(self.cax.get_clim())
+        if self.canvas is not None:
+            self.canvas.draw()
+
+    def update_matplotlib_fig(self):
+        """
+        Calls the appropriate method to generate the current
+        spectrogram of frequency plot.
+        :return:
+        """
+        if self.current_view == "spec":
+            self.create_specgram()
+        else:
+            self.create_plot()
 
     def on_play(self):
         """
@@ -285,6 +387,7 @@ class BeeApp(Tk.Tk):
                     with open(temp.name, 'wb') as r:
                         ftp.retrbinary('RETR ' + audio_dir + audio_file, r.write)
                     rate, wav = get_data(temp.name)
+                    os.system("open " + temp.name)
                     self.audio_files[self.pit][audio_file] = (rate, wav)
                 else:
                     rate, wav = self.audio_files[self.pit][audio_file]
@@ -292,6 +395,49 @@ class BeeApp(Tk.Tk):
                 thread.start_new_thread(player, (self.play, rate, wav))
             else:
                 self.message.config(text="No audio file.")
+
+    def on_play_video(self):
+        """
+        Callback for play button. Locates the file associated with given time if the file exists.
+        Then calls the play function to play the located file.
+        :return: None
+        """
+        audio_dir = None
+        audio_file = None
+        for i in range(0, 60):
+            sec = int(self.center, 16)
+            sec -= i
+            sec = format(sec, 'x')
+            date, time = Dates.to_date(sec)
+            date = date.split('-')
+            date.reverse()
+            date = '-'.join(date)
+            try:
+                ftp.cwd(self.video_dir % date)
+                files = ftp.nlst()
+                print files
+                if files is not None:
+                    for f in files:
+                        if time in f or '-'.join(time.split(':')) in f:
+                            audio_dir = self.video_dir % date
+                            audio_file = f
+                            break
+            except ftplib.error_perm:
+                pass
+            if audio_file is not None:
+                break
+        if audio_file is not None:
+            print audio_dir + audio_file
+            if audio_file not in self.audio_files[self.pit]:
+                filename, file_extension = os.path.splitext(audio_file)
+                temp = tempfile.NamedTemporaryFile(suffix=file_extension)
+                temp.close()
+                with open(temp.name, 'wb') as r:
+                    ftp.retrbinary('RETR ' + audio_dir + audio_file, r.write)
+                os.system("open " + temp.name)
+        else:
+            self.message.config(text="No video file.")
+
 
     def on_right(self):
         """
@@ -301,7 +447,8 @@ class BeeApp(Tk.Tk):
         cen = int(self.center, 16)
         cen += 2 ** (3 + self.zoom) / 2
         self.center = format(cen, 'x')
-        self.get_next_16(self.center)
+        self.update_combined_spec(self.center)
+        self.update_matplotlib_fig()
 
     def on_left(self):
         """
@@ -311,21 +458,22 @@ class BeeApp(Tk.Tk):
         cen = int(self.center, 16)
         cen -= 2 ** (3 + self.zoom) / 2
         self.center = format(cen, 'x')
-        self.get_next_16(self.center)
+        self.update_combined_spec(self.center)
+        self.update_matplotlib_fig()
 
-    def get_next_16(self, hex_num):
+    def update_combined_spec(self, hex_num):
         """
-        Gets the next 16 files surrounding the specified hex time stamp.
+        Gets the 16 files surrounding the specified hex time stamp.
         Center time should be the time passed in.
         :param hex_num: hex time stamp that will be the center of the data
         :return: None
         """
         print ("Zoom: ", self.zoom)
         combined_spec = None
-        num = int(hex_num, 16)
-        num -= 2 ** (3 + self.zoom) / 2
+        lm = int(hex_num, 16)
+        lm -= 2 ** (3 + self.zoom) / 2  # leftmost value
         for i in range(0, 2 ** (3 + self.zoom), 2 ** (self.zoom - 1)):
-            i_hex = '{:08x}'.format(int(num + i))
+            i_hex = '{:08x}'.format(int(lm + i))
             i_hex = i_hex[:int(len(i_hex) - ((self.zoom - 1) / 4))]
             i_dir = "/".join(i_hex[:-1]) + "/"
             i_file = None
@@ -369,54 +517,7 @@ class BeeApp(Tk.Tk):
                 else:
                     combined_spec = np.vstack((combined_spec, [0] * 2049))
         self.combined_spec = combined_spec
-        hex_time1 = '{:08x}'.format(int(num))
-        hex_time2 = '{:08x}'.format(int(num + 2 ** (3 + self.zoom) - 1))
-        self.leftmost = make_hex8(hex_time1)
-        self.create_fig(combined_spec, hex_time1, hex_time2)
-        if self.current_view != "spec":
-            self.plot.config(text="Frequencies")
-            self.current_view = "spec"
-
-    def create_fig(self, combined_spec, hex_time1, hex_time2):
-        """
-        Creates the current spectrogram.
-        :param combined_spec: spectrogram to be displayed
-        :param hex_time1: start time of spectrogram
-        :param hex_time2: end time of spectrogram
-        :return: None
-        """
-        date1, file_time1 = Dates.to_date(hex_time1)
-        date2, file_time2 = Dates.to_date(hex_time2)
-        if not date1 == date2:
-            title = date1 + "  -  " + date2
-        else:
-            title = date1
-        if self.fig is None:
-            fig, ax = plt.subplots()
-            fig.canvas.draw()
-            self.fig = fig
-            self.ax = ax
-        self.ax.clear()
-        self.ax.set_xticks(np.arange(0, combined_spec.shape[0], 1.0))
-        self.ax.set_xticklabels(["" for x in range(combined_spec.shape[0])])
-        cax = self.ax.imshow(20 * np.log10(combined_spec.T), origin='lower', aspect='auto')
-        self.ax.set_title(title)
-        labels = [item.get_text() for item in self.ax.get_xticklabels()]
-        labels[0] = file_time1
-        center_date, center_time = Dates.to_date(self.center)
-        labels[8] = center_time
-        labels[len(labels) - 1] = file_time2
-        self.ax.set_xticklabels(labels)
-        yfmt = tkr.FuncFormatter(numfmt)
-        self.ax.yaxis.set_major_formatter(yfmt)
-        # sets the colorbar for all spectrograms
-        if self.cax is None and np.count_nonzero(combined_spec) != 0:
-            self.cax = self.ax.imshow(20 * np.log10(combined_spec.T), origin='lower', aspect='auto')
-            self.fig.colorbar(cax)
-        if self.cax is not None:
-            cax.set_clim(self.cax.get_clim())
-        if self.canvas is not None:
-            self.canvas.draw()
+        self.leftmost = make_hex8('{:08x}'.format(lm))
 
 
 def numfmt(y, pos):
@@ -472,7 +573,7 @@ def player(play_button, rate, wav):
     """
     play_button.config(text="Stop")
     p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(2), channels=1, rate=rate, output=True)
+    stream = p.open(format=p.get_format_from_width(2), channels=1, rate=44100, output=True)
     stream.start_stream()
     start = 0
     end = 1024
@@ -500,8 +601,21 @@ def on_closing(app):
 def main():
     root = Tk.Tk()
     root.withdraw()
-    d = DateDialog(root)
+    while True:
+        try:
+            login = LoginDialog(root)
+            global user, password
+            user, password = login.result
+            global ftp
+            ftp = ftplib.FTP('cs.appstate.edu', user, password)
+            login.destroy()
+            break
+        except ftplib.error_perm:
+            tkMessageBox.showwarning(
+            "Login Failure", "Incorrect login credentials.\nPlease try again.")
+            pass
     try:
+        d = DateDialog(root)
         date, time, pit, channel = d.result
         root.destroy()
         app = BeeApp(date, time, pit, channel)
